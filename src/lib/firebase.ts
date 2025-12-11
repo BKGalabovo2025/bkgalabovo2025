a00deffeeee6a25f6812f1bb56e2bca08d5a8bb9
Бадминтон клуб Гълъбово <bkgalabovo2014@gmail.com>
2025-12-11 08:48:31 +0000

    Fix Firebase config for production

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,9 +6,9 @@ const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "580356814576",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: "G-ZZ52PG2NMX"
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Инициализация на Firebase
