72dfa99fc9d67c591f392754ae428a02a1db85d2
Бадминтон клуб Гълъбово <bkgalabovo2014@gmail.com>
2025-12-19 02:37:52 +0000

    fix: Use environment variables for Firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,16 @@
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtSQyDPUolZDDgtpwvUH02UZU2wynq1cQ",
-  authDomain: "bkgalabovo2025.firebaseapp.com",
-  projectId: "bkgalabovo2025",
-  storageBucket: "bkgalabovo2025.appspot.com", // Corrected to a more standard format
-  messagingSenderId: "580356814576",
-  appId: "1:580356814576:web:7bf7d6847f6a8d9431b435",
-  measurementId: "G-ZZ52PG2NMX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
