45a94eda14c1537133bdde7a5d8478918aebb93f
Бадминтон клуб Гълъбово <bkgalabovo2014@gmail.com>
2025-12-21 08:03:33 +0000

    Add Firebase environment variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -33,6 +33,14 @@ const nextConfig = {
     ],
   },
   allowedDevOrigins: ["3001-firebase-studio-1764924832659.cluster-bbaw5ubmyzaeiswfik7bsgqew2.cloudworkstations.dev"],
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  }
 };
 
 module.exports = nextConfig;
