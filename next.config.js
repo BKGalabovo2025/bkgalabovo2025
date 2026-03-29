0d4eb49395eec2a7206414f28cc0fd7ba109f075
BKGalabovo2025 <bkgalabovo2014@gmail.com>
2026-03-29 12:04:59 +0300

    final: optimize next.config and cleanup build warnings

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // This allows access from Cloud Workstations URLs which are dynamic.
-    // We move these to environment variables as per audit recommendations.
-    allowedRevalidateHeaderKeys: ['x-prerender-revalidate'],
-  },
-  // In Next.js 14/15, allowedDevOrigins might need to be in 'experimental'
-  // using a process environment variable for these specific origins.
-  ...(process.env.ALLOWED_DEV_ORIGINS ? {
-    allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS.split(','),
-  } : {
-    allowedDevOrigins: [
-      "*.cloudworkstations.dev",
-    ]
-  }),
+  experimental: {},
+  allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS 
+    ? process.env.ALLOWED_DEV_ORIGINS.split(',') 
+    : ["*.cloudworkstations.dev"],
 };
 
 module.exports = nextConfig;
