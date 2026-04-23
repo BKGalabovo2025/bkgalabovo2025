efcf20160203128d8a4c6a534de246982c55bc9f
bkgalabovo2014 <bkgalabovo2014@gmail.com>
2026-04-23 19:41:36 +0000

    Feat: Основи на модул Турнири и отстраняване на 404

---
+++ src/app/(protected)/tournaments/page.tsx
@@ -0,0 +1,8 @@
+export default function TournamentsPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Турнири</h1>
+      <p>Тук ще се намира функционалността за управление на турнири.</p>
+    </div>
+  );
+}
