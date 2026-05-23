7678d71e6e2d6071c52b57c5043be5300d498bc0
BK Galabovo <bkgalabovo2014@gmail.com>
2026-05-23 10:03:01 +0300

    fix: make protected layout dynamic to resolve next.js warnings

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -3,6 +3,8 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import ProtectedLayoutClient from "./ProtectedLayoutClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function ProtectedLayout({
   children,
 }: {
